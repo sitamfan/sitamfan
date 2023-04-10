eb1f255a0a5738603c82f5c093f95c162c910e14
AKBAR\akbar <akbarmulyahakbar89001@gmail.com>
2023-04-10 10:38:15 +0700

    first

---
+++ Downloads/TugasTekweb1/TugasTekweb1/application/views/BioAyu.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Biodata Ayu</title>
+</head>
+<body>
+
+<h2>Nama : Ayu Nur Oktaviani</h2>
+<h2>NIM : 3411201047</h2><br/>
+
+</body>
+</html>
---
+++ Downloads/TugasTekweb1/TugasTekweb1/application/views/Biodata.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Biodata Kelompok</title>
+</head>
+<body>
+
+<h1>MEET OUR TEAM</h1><br/>
+<a href="http://localhost/TugasTekweb1/index.php/BiodataAyu">Ayu</a></br>
+<a href="http://localhost/TugasTekweb1/index.php/BiodataAkbar">Akbar</a></br>
+<a href="http://localhost/TugasTekweb1/index.php/BiodataRiza">Riza</a></br>
+</body>
+</html>
